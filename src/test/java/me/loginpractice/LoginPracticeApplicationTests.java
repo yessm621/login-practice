796c0ba492263efc87f3277e180b6e3540a5b852
yessm621 <yessm621@gmail.com>
2023-08-23 16:25:09 +0900

    first commit

---
+++ src/test/java/me/loginpractice/LoginPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.loginpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
